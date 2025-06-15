e95d4b17c35d8e45ad75baebeca9e41083ba1410
thiendekaco <thien.bt214099@sis.hust.edu.vn>
2025-06-15 22:50:16 +0700

    feat: update code #6

--- include/blynk_instance.h
+++ include/blynk_instance.h
@@ -1,24 +1,17 @@
-#ifndef BLYNK_INSTANCE_H
-#define BLYNK_INSTANCE_H
+#pragma once
+  #ifndef BLYNK_INSTANCE_H
+  #define BLYNK_INSTANCE_H
 
-// When including this header from regular modules, ensure the Blynk
-// library does not create its own global instance. Only the implementation
-// file defines the object by defining BLYNK_INSTANCE_IMPLEMENTATION.
-#ifndef BLYNK_INSTANCE_IMPLEMENTATION
-#ifndef BLYNK_NO_GLOBAL
-#define BLYNK_NO_GLOBAL
-#endif
-#endif
+// Prevent the Blynk header from declaring its own global instance so we
+// can provide one manually in a single .cpp file.
+  #ifndef BLYNK_NO_GLOBAL
+  #define BLYNK_NO_GLOBAL
+  #endif
 
-#include <BlynkSimpleEsp32.h>
+  #include <BlynkSimpleEsp32.h>
 
-#ifdef BLYNK_INSTANCE_IMPLEMENTATION
-// In the implementation file we let the library create the global object
-// by including the header without BLYNK_NO_GLOBAL defined.
-#else
-// Other modules only reference the instance defined elsewhere
-extern BlynkWifi Blynk;
-#endif
+// Shared Blynk object used across the application.  The instance is
+// defined in blynk_instance.cpp.
+  extern BlynkWifi Blynk;
 
-
-#endif // BLYNK_INSTANCE_H
+ #endif // BLYNK_INSTANCE_H
--- src/blynk_instance.cpp
+++ src/blynk_instance.cpp
@@ -1,11 +1,13 @@
-#define BLYNK_PRINT Serial
 
-// Only this translation unit should create the global Blynk object. All other
-// modules include `blynk_instance.h` which defines `BLYNK_NO_GLOBAL` to prevent
-// the Blynk library from declaring its own instance.  Here we explicitly remove
-// that define and mark this file as the implementation unit.
-#define BLYNK_INSTANCE_IMPLEMENTATION
-#ifdef BLYNK_NO_GLOBAL
-#undef BLYNK_NO_GLOBAL
-#endif
-#include "blynk_instance.h"
+  #define BLYNK_PRINT Serial
+
+// This file provides the single global Blynk instance for the entire
+// project.  All other modules include `blynk_instance.h` which keeps the
+// library from defining its own object.
+ #include "blynk_instance.h"
+
+// Replicate the default objects normally created by the Blynk header but
+// keep them local to this translation unit.
+static WiFiClient blynkWiFiClient;
+static BlynkArduinoClient blynkTransport(blynkWiFiClient);
+BlynkWifi Blynk(blynkTransport);
